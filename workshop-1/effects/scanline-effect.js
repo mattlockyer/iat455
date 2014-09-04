d2ca4bd119c3490ff5eba1f0c44c0eb63f02bacd
Matt Lockyer <mattdlockyer@gmail.com>
2014-09-04 15:43:12 -0700

    finished workshop-1 code

--- workshop-1/effects/scanline-effect.js
+++ workshop-1/effects/scanline-effect.js
@@ -7,12 +7,13 @@ var ScanlineEffect = function(canvas) {
 	//parameters
 	self.canvas = canvas;
 	self.interval = null;
+	self.y = 0;
 	//controls
 	self.name = 'ScanlineEffect';
 	self.controls = {
 		Toggle:APP.Effect.toggle.bind(this),
 		Thickness:{
-			value:1,
+			value:2,
 			min:1,
 			max:32,
 			step:1
@@ -32,15 +33,19 @@ ScanlineEffect.prototype = {
 		thickness = this.controls.Thickness.value,
 		scrollSpeed = this.controls.Scrollspeed.value,
 		w = MEDIA.width,
-		h = MEDIA.height;
+		h = MEDIA.height,
+		lineWidth = w * 4;
+
+		this.y += scrollSpeed;
+		if (this.y > thickness * 2) this.y = 0;
 
 		APP.drawImage(canvas);
 
 		var img = canvas.getImageData(),
 		data = img.data;
 
-		for (var i = 0; i < data.length; i += w * 2 * 4 * thickness) {
-			for (var j = 0; j < w * 4 * thickness; j++) {
+		for (var i = this.y * lineWidth; i < data.length; i += lineWidth * 2 * thickness) {
+			for (var j = 0; j < lineWidth * thickness; j++) {
 				data[i + j] = 0;
 			}
 		}
