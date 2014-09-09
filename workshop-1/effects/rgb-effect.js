7dd906c5edaf4517fed629515129e08c15037cb6
Matt Lockyer <mattdlockyer@gmail.com>
2014-09-09 13:28:37 -0700

    updated rgb effect added performance test between direct and array pixel access

--- workshop-1/effects/rgb-effect.js
+++ workshop-1/effects/rgb-effect.js
@@ -8,6 +8,11 @@ var RGBEffect = function() {
 	self.canvas = new MEDIA.Canvas();
 	//controls
 	self.name = 'RGBEffect';
+
+	self.directTotal = 0;
+	self.arrayTotal = 0;
+	self.count = 0;
+
 	self.controls = {
 		RedThreshold:{
 			value:128,
@@ -42,8 +47,33 @@ RGBEffect.prototype = {
 		APP.drawImage(canvas);
 
 		var img = canvas.getImageData(),
-		data = img.data;
+		data = img.data,
+		data32 = new Uint32Array(img.data.buffer);
 
+		/*******************************
+		* Two different approachings for manipulating pixels, the result is exactly the same
+		*******************************/
+
+		/*******************************
+		* Approach 1 (fastest): direct pixel manipulation using 32 bit uint array
+		*******************************/
+
+		var t = Date.now();
+		for (var i = 0; i < data32.length; i++) {
+			var pixel = data32[i];
+			var r = (pixel) & 0xff,
+			g = (pixel >> 8) & 0xff,
+			b = (pixel >> 16) & 0xff;
+
+			data32[i] = (255 << 24) | ((b > bThresh ? b : 0) << 16) | ((g > gThresh ? g : 0) << 8) | (r > rThresh ? r : 0);
+		}
+		this.directTotal += Date.now() - t;
+
+		/*******************************
+		* Approach 2: using imageData.data array directly
+		*******************************/
+
+		var t = Date.now();
 		for (var i = 0; i < data.length; i += 4) {
 			var r = data[i],
 			g = data[i + 1],
@@ -53,6 +83,13 @@ RGBEffect.prototype = {
 			data[i + 1] = g > gThresh ? g : 0;
 			data[i + 2] = b > bThresh ? b : 0;
 		}
+		this.arrayTotal += Date.now() - t;
+
+
+		//Benchmarking both techniques
+		this.count++;
+		console.log('Direct Average: ' + (this.directTotal / this.count));
+		console.log('Array Average: ' + (this.arrayTotal / this.count));
 
 		canvas.putImageData(img);
 	}
