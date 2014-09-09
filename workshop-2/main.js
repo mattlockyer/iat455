2a7370b8ac58b6a788a7087d84c092694aad4092
Matt Lockyer <mattdlockyer@gmail.com>
2014-09-09 13:42:04 -0700

    added workshop-2 folder

---
+++ workshop-2/main.js
@@ -0,0 +1,17 @@
+
+
+"use strict";
+
+var APP = APP || {};
+
+APP.init = function() {
+	//setup app
+	APP.setup({w:640, h:480});
+	//add effects
+	APP.effects = [];
+	APP.effects.push(new RGBEffect());
+	//setup controls
+	APP.setupControls();
+};
+
+APP.init();
